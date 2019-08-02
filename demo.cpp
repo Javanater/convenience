9a30e5cea6d0f6388c8da6f5346315bddf0fd8c4
rfortenberry <fortenberry.rm@protonmail.com>
2019-08-01 21:48:56 -0400

    removed range-v3. needs c++17...

--- demo.cpp
+++ demo.cpp
@@ -10,19 +10,7 @@ int main()
     vector<int> v;
     v += 3,5,9;
 
-    for_each(v | enumerate, [](const auto& t)
-    {
-        cout << "hello" << endl;
-    });
-
-    size_t i;
-    int e;
-    RANGES_FOR(tie(i, e), v | enumerate)
-        cout << i << ',' << e << endl;
-
     for_each(v, cout << _1 << endl);
 
-    for_each(ints(1,3), cout << _1 << endl);
-    
     cout << steady_clock::now() << endl;
 }
